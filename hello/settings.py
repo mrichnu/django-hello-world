5759019210644836ae800ebb14f89c6c91d6d9b8
Matthew Rich <m-rich@northwestern.edu>
2015-10-08 17:08:03 -0500

    set STATIC_ROOT

--- hello/settings.py
+++ hello/settings.py
@@ -102,5 +102,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 
 STATIC_URL = '/static/'
